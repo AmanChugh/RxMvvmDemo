6b6e783ef0b8049cfe5b06be24c27d0e6424043d
Aman Deep <aman.deep@digispice.com>
2019-01-23 13:39:41 +0530

    Add Application class to manifest.

--- app/src/main/java/com/digispice/mvvmdemo/MainActivity.kt
+++ app/src/main/java/com/digispice/mvvmdemo/MainActivity.kt
@@ -17,9 +17,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         AndroidInjection.inject(this)
+        setContentView(R.layout.activity_main)
+
         cryptocurrenciesViewModel = ViewModelProviders.of(this, cryptocurrenciesViewModelFactory).get(
             CryptocurrenciesViewModel::class.java
         )
